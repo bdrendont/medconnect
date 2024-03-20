a81f763629f63982f4a60406fcfe8b96b17cdd21
Jeninson Peralta <jeninson@gmail.com>
2024-03-19 21:08:54 -0500

    Agregando libreria JQuery 3.7.1

---
+++ control/login.php
@@ -0,0 +1,5 @@
+<?php
+    echo "estas en Control del Login <br/>";
+    //echo $_POST["correo"]." ".$_POST["contrasena"]
+    echo $_GET["correo"]." ".$_GET["contrasena"]
+?>
--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@
 function ruta(url){
     location.href=url
 }
+
+$("#btn-iniciar").on("click",()=>{
+    $("#form_login").submit()
+})
+
+$("#btnnueva").on("click",()=>{
+    ruta("newaccount.html")
+})
+
+$("#form_login").on("submit",()=>{
+    //alert("ON Submit del Formulario");
+    let data = $("#form_login").serialize()
+    alert("Datos ha enviar\n\n"+data)
+    ruta("../control/login.php")
+    return false
+    //Ajax
+})
+//$(".botoninicio").fadeOut()
